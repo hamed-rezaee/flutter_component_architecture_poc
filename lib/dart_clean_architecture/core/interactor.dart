ecdda169f17c3738eec4a3da109a6a4b18bcb95b
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-08-10 11:49:46 +0800

    minor refactor

--- lib/dart_clean_architecture/core/interactor.dart
+++ lib/dart_clean_architecture/core/interactor.dart
@@ -1,7 +1,11 @@
 import 'domain.dart';
 
 abstract class BaseService {
-  const BaseService(this.repository);
+  const BaseService();
+}
+
+abstract class BaseServiceWithRepository implements BaseService {
+  const BaseServiceWithRepository(this.repository);
 
   final BaseRepository repository;
 }
